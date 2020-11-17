7c685d0613da07fd6477fcd940fd4b986e1a7ce8
mirr254 <sammysteppa90@gmail.com>
2020-11-17 21:02:34 +0300

    fix bootup script

--- bright-events-api/scripts/boot.sh
+++ bright-events-api/scripts/boot.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-
-source venv/bin/activate
-flask db upgrade
-flask translate compile
+#!/bin/bash
+source /app/venv/bin/activate
+python manage.py db migrate
+python manage.py db upgrade
 exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app
+# equivalent to "from myproject import run as app"
